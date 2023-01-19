ab3abb095f895bc11d533c69c6aabb46c51532f5
Tetiankaa <tanyakorsun0208@gmail.com>
2023-01-19 20:44:41 +0100

    hw1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {Simpsons} from "./components/Simpsons/Simpsons";
+import {Characters} from "./components/Characters/Characters"
 
 const App = () => {
     return (
         <div>
             <Simpsons/>
+            <Characters/>
         </div>
     );
 };
